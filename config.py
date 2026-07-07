01bd1b0dab6747ba2a80010a5a5f2c8eaf047719
WhtKswx55 <whtkswx@gamil.com>
2026-07-07 23:45:28 +0600

    upd links 2.0

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8561266872:AAEBnYLtMwN9PryjqqT7ymr6rQVioRuPvU8")
 ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "7240769536").split(",") if x.strip()]
-WEBAPP_URL = os.getenv("WEBAPP_URL", "https://booking-bot-i204.onrender.com/webapp/")  # например https://your-app.onrender.com/webapp/
+WEBAPP_URL = os.getenv("WEBAPP_URL", "https://booking-bot-i204.onrender.com/webapp/index.html")  # например https://your-app.onrender.com/webapp/
 BASE_URL = os.getenv("BASE_URL", "https://booking-bot-i204.onrender.com")      # тот же домен, нужен для self-ping
 PORT = int(os.getenv("PORT", "8080"))
 
